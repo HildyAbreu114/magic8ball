46e0b66b92f39de1ab862f5b0452f772d927c8d1
hildyabreu114 <hildyabreu@pursuit.org>
2020-05-24 21:48:32 -0400

    create xcode project

---
+++ magic8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic8ball
+//
+//  Created by hildy abreu on 5/24/20.
+//  Copyright © 2020 hildy abreu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
